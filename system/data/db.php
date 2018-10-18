abb16aa1a736ad72b18793f5b78dead3f9e4c179
Artem <fkaparov@inbox.ru>
2018-10-18 21:37:48 +0300

    sait
    wewqe

---
+++ system/data/db.php
@@ -0,0 +1,16 @@
+<?php
+
+define ("DBHOST", "localhost");
+
+define ("DBNAME", "suka");
+
+define ("DBUSER", "suka");
+
+define ("DBPASS", "");
+
+define ("PREFIX", "vii");
+
+define ("COLLATE", "utf8");
+
+$db = new db; 
+?>
